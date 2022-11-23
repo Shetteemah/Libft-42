f6f433401201a0a6162903e4e05864e799ea4b29
Shetima kyari Ali <shettimaali58@yahoo.com>
2022-11-23 21:59:02 +0100

    added strlen strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 15:57:14 by sheali            #+#    #+#             */
+/*   Updated: 2022/04/22 03:59:27 by sheali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src,
+size_t dstsize)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (dstsize != '\0')
+	{
+		while (src[i] != '\0' && i < dstsize - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
