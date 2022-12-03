408a69e1b473c18dcf820df6d4e56a24c05f5ef7
Shetima kyari Ali <shettimaali58@yahoo.com>
2022-12-04 00:53:07 +0100

    added ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 01:41:51 by sheali            #+#    #+#             */
+/*   Updated: 2022/04/21 17:47:08 by sheali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, (count * size));
+	return (ptr);
+}
