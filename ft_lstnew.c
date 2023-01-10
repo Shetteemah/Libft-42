0264bb03673f345cdd84a545896b9dc28ee65127
Shetima kyari Ali <shettimaali58@yahoo.com>
2023-01-10 20:31:41 +0100

    ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/15 00:31:32 by sheali            #+#    #+#             */
+/*   Updated: 2022/04/21 19:45:52 by sheali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*element;
+
+	element = malloc(sizeof(t_list));
+	if (!(element))
+		return (NULL);
+	element->content = content;
+	element->next = NULL;
+	return (element);
+}
