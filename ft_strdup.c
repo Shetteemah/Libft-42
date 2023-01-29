2f919078d02a5d18d541d26edb869daf89ebb285
Shetima kyari Ali <shettimaali58@yahoo.com>
2023-01-29 22:49:52 +0100

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 00:41:50 by sheali            #+#    #+#             */
+/*   Updated: 2022/04/24 01:21:59 by sheali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int		i;
+
+	dest = (char *) malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (dest == 0)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
