92df49fb193d0e435629ab770ad883d171d1adea
Shetima kyari Ali <shettimaali58@yahoo.com>
2023-02-10 23:10:15 +0100

    ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 03:25:59 by sheali            #+#    #+#             */
+/*   Updated: 2022/04/21 01:38:10 by sheali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*result;
+	unsigned int	i;
+
+	if (s == NULL)
+		return (NULL);
+	result = (char *) malloc(ft_strlen(s) +1);
+	if (!(result))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = (*f)(i, s[i]);
+		i++;
+	}
+	result[ft_strlen(s)] = '\0';
+	return (result);
+}
